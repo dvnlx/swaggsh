0170596329b0610c670473a6e5ab42dfc97fdd49
iDevYT <alphangred57@gmail.com>
2022-08-15 18:53:15 +0200

    init

---
+++ swagg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Enter your name for the SWAGG app"
+
+read user
+
+sleep 1
+
+echo "Enter Your Distro e.g fddora,arch"
+
+read distro
+
+clear
+neofetch --ascii_distro $distro
+
+sleep 1
+
+echo "hey $user"
+
+sleep 1
+
+echo "i use $distro btw"
+exit
